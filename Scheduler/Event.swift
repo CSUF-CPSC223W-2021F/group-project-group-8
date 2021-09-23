f896776452a43a35f3dd100934103abc0addd037
Henry Torres <htorres572@csu.fullerton.edu>
2021-09-23 04:24:18 -0700

    making Event branch

---
+++ Scheduler/Event.swift
@@ -0,0 +1,14 @@
+//
+//  Event.swift
+//  Scheduler
+//
+//  Created by Henry on 9/23/21.
+//
+
+import Foundation
+
+struct EventStruct {
+    var eventName: String = ""
+    var location: String = ""
+    var time: String = ""
+}
