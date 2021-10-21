408f300c393dc846a19ae77a33536dfaa5e61dfe
Nancy Badillo <n_badillo@csu.fullerton.edu>
2021-10-21 12:59:21 -0700

    Updated log-in screen

--- Scheduler/Login.swift
+++ Scheduler/Login.swift
@@ -7,9 +7,7 @@
 
 import Foundation
 
-struct Login {
+struct CreateAccount {
     var username: String = ""
     var password: String = ""
 }
-
-// Need to actually find a way to store values and log out, at the moment just have what the struct values could be.  Might need to create a "sign up" struct.
