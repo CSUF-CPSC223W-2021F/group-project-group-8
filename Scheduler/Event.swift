104a9744b281f298520a6261c9820d8511d6dd44
Henry Torres <htorres572@csu.fullerton.edu>
2021-11-18 13:49:38 -0800

    created a event class and a event list viewer

---
+++ Scheduler/Event.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Scheduler
+//
+//  Created by Henry on 11/18/21.
+//
+
+import Foundation
+
+class Event: Codable {
+    var event: String = ""
+    var done: Bool = false
+    var date: String = ""
+    
+}
