02f976d7a841badfeb2636c296fc655a8ca42b73
Nancy Badillo <n_badillo@csu.fullerton.edu>
2021-12-02 16:19:47 -0800

    Signup Form completed, should transition to Home page.

---
+++ Scheduler/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Scheduler
+//
+//  Created by Karina Badillo on 12/2/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        static let homeViewController = "HomeViewController"
+    }
+}
